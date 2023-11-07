cd84b9731b0accbca97358905a8f518564a8a7ee
unknown <shajishibin425@gmail.com>
2023-11-07 07:55:12 +0530

    added

--- DemoShop-main/ecommerce_in/ecommerce_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from ecommerce_app import views
-app_name='ecommerce_app'
-urlpatterns = [
-    path('',views.allProdCat,name='allProdCat'),
-    path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
-    path('<slug:c_slug>/<slug:product_slug>/', views.proDetail, name='prodCatdetail'),
-]
